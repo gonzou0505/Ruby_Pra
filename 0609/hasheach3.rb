89e2f5a95ee3b46f4ce178d93a78a21749ab9c2c
gonzou0505 <koro05050525@gmail.com>
2020-06-10 02:21:22 +0900

    change

--- 0609/hasheach3.rb
+++ 0609/hasheach3.rb
@@ -9,7 +9,7 @@ characters.each do |character|
   puts "--------------------"
   puts "名前は#{character[:name]}です"
   
-  # キー:ageの値に応じて年齢の情報を出力してください
+  # キー:ageの値に応じて年齢の情報を出力してください。
   if character[:age]
     puts "年齢は#{character[:age]}歳です"
   else
